f8f1e2ae5e8140dc4ac9be6f06c5d5714889b1c3
Payten Giles <payten.giles@gmail.com>
2018-03-30 13:55:32 +1100

    Create batch-happy version of assignfileuuids_v0.0

--- src/MCPClient/lib/batch_development.py
+++ src/MCPClient/lib/batch_development.py
@@ -1,7 +1,7 @@
 converted_modules = {
+    'assignfileuuids_v0.0': 'assign_file_uuids',
     'removeunneededfiles_v0.0': 'remove_unneeded_files',
 }
 
 modules_under_development = [
-    'remove_unneeded_files',
 ]
