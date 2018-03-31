b9d7dcf82f4c7e40cac9c20c173cfc2847afc877
jambun <jambun@gmail.com>
2018-03-31 23:26:28 +1100

    Batch my Clamscan

--- src/MCPClient/lib/batch_development.py
+++ src/MCPClient/lib/batch_development.py
@@ -1,6 +1,7 @@
 converted_modules = {
     'assignfileuuids_v0.0': 'assign_file_uuids',
     'removeunneededfiles_v0.0': 'remove_unneeded_files',
+    'archivematicaclamscan_v0.0': 'archivematica_clamscan',
 }
 
 modules_under_development = [
