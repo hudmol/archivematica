d36c4859558d232eb792e3e56b73b6505ec3d001
Payten Giles <payten.giles@gmail.com>
2018-04-03 16:10:29 +1000

    Batch bind_pid client script

--- src/MCPClient/lib/batch_development.py
+++ src/MCPClient/lib/batch_development.py
@@ -1,5 +1,6 @@
 converted_modules = {
     'assignfileuuids_v0.0': 'assign_file_uuids',
+    'bindpid_v0.0': 'bind_pid',
     'removeunneededfiles_v0.0': 'remove_unneeded_files',
     'archivematicaclamscan_v0.0': 'archivematica_clamscan',
 }
